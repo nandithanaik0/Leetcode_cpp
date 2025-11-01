ca85855a76770bd7ca6cb7cc96bdac1ce8408cad
Nanditha Naik <82931456+nandithanaik0@users.noreply.github.com>
2025-11-01 11:14:43 -0500

    Two sum brute force

---
+++ 1_two_sum_bf.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output;
+
+        for(int i = 0; i < nums.size(); i++){
+            for (int j = i + 1; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target){
+                    output.push_back(i);
+                    output.push_back(j);
+                }
+            }
+        }
+        return output;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    cout << "Indices: ";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
